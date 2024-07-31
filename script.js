66a38b6fb48bc087708ff7a2a359932d428ba155
nicolasgomez2001 <nicolaseduardo.gomez@globant.com>
2024-07-31 10:52:03 -0500

    Proyecto Final de Google

---
+++ script.js
@@ -0,0 +1,18 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const searchInput = document.getElementById('search-input');
+    const clearIcon = document.getElementById('clear-icon');
+
+    searchInput.addEventListener('input', function() {
+        if (searchInput.value.length > 0) {
+            clearIcon.style.display = 'block';
+        } else {
+            clearIcon.style.display = 'none';
+        }
+    });
+
+    clearIcon.addEventListener('click', function() {
+        searchInput.value = '';
+        clearIcon.style.display = 'none';
+        searchInput.focus();
+    });
+});
